43bee866dddb5493ecb622a872298fc8d8896e5b
CG <caseygrote@gmail.com>
2013-12-13 14:28:49 -0500

    initial commit; adding basics of structure, layout etc

---
+++ LapinLeap/Room.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LapinLeap
+{
+    /// <summary>
+    /// Interaction logic for Room.xaml
+    /// </summary>
+    public partial class Room : UserControl
+    {
+        public Room()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            BGgrid.Background = Brushes.Blue;
+            //CENTER THIS!
+            //CHANGE TIME; CHANGe MAINGRID BG
+            //CHANGE CHARA LOC
+        }
+    }
+}
